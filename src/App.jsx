c1222e683fdedf662907069e952750226761cf92
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2024-04-06 14:23:44 -0700

    Publishing to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
   return (
     <BrowserRouter>
       <Nav />
+      <Home />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -6,9 +6,9 @@ function Nav() {
     <div className="navbar-container">
       <h1><Link to="/" className="JHill">J Hill</Link></h1>
       <nav className="navbar">
-        <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
-        <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink>
-        <NavLink to="/projects" className="nav-link" activeClassName="active">Projects</NavLink>
+        <NavLink to="/" className="nav-link" activeclasscame="active">Home</NavLink>
+        <NavLink to="/about" className="nav-link" activeclasscame="active">About</NavLink>
+        <NavLink to="/projects" className="nav-link" activeclasscame="active">Projects</NavLink>
       </nav>
     </div>
   );
